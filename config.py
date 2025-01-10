4a877b46896afd299931823453df78191a2b7f67
Serj8772 <icq66623702@ya.ru>
2025-01-10 16:31:01 +0300

    initial

---
+++ config.py
@@ -0,0 +1,24 @@
+# DATABASE SETTINGS
+host = ''
+user = ''
+password = ''
+database = ''
+
+# размер спреда для вывода
+min_spread = 0.5 # установим минимальный спред
+max_spread = 100 # установим максимальный спред
+
+
+
+
+def links_for_pairs(cex, pair_for_link):
+    if cex == 'binance':
+        return f'https://www.binance.com/en/trade/{pair_for_link.replace("USD", "_USD")}'
+    elif cex == 'gateio':
+        return f'https://www.gate.io/trade/{pair_for_link.replace("USD", "_USD")}'
+    elif cex == 'bybit':
+        return f'https://www.bybit.com/en/trade/spot/{pair_for_link.replace("USD", "/USD")}'
+    else:
+        print('Ошибка названия биржи')
+
+# print(f'https://www.binance.com/en/trade/{i[0].replace("USD", "_USD")} https://www.gate.io/trade/{i[0].replace("USD", "_USD")}\n\n')
